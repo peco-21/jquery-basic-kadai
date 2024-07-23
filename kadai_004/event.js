a940213d18ebca5fdebaf1f247683d88702f6191
peco-21 <peco.21.mhe@gmail.com>
2024-07-24 08:18:24 +0900

    修正
    jsファイル修正

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,9 +1,8 @@
-$(document).ready(function() {
-  // loadイベントの処理
-  console.log("loadイベントが発生しました");
-  
-  // scrollイベントの処理
-  $(window).on('scroll', function() {
-    console.log("scrollイベントが発生しました");
-  });
+$(window).on('load', () => {
+  console.log('loadイベントが発生しました');
 });
+
+$(window).on('scroll', () => {
+  console.log('scrollイベントが発生しました');
+});
+
